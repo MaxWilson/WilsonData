46118bde273b990b76bdc752b1b9150632645da8
Max Wilson <maxw@microsoft.com>
2018-10-25 13:16:05 -0700

    Add auth (but wip, not working for Save via POST)

--- Library.fs
+++ Library.fs
@@ -9,6 +9,8 @@ module API =
     open Newtonsoft.Json
     open System.IO
     open System
+    open System.Threading
+    open System.Security.Claims
 
     let ofReq<'t when 't: equality and 't :> obj>(req: HttpRequest) =
       try
@@ -30,11 +32,28 @@ module API =
 
     let mutable store = Map.empty
 
+    let ident (req: HttpRequest) =
+      let id =
+        match req.Headers.TryGetValue("X-MS-CLIENT-PRINCIPAL-ID") with
+        | true, v ->
+          v.[0]
+        | _ -> "Unknown"
+      let name =
+        match req.Headers.TryGetValue("X-MS-CLIENT-PRINCIPAL-NAME") with
+        | true, v ->
+          v.[0]
+        | _ -> "Unknown"
+      sprintf "%s\\%s" id name
+
+    let myStores req =
+      let me = ident req
+      store |> Map.filter (fun _ (v: DynamicStorageRow) -> v.owner = me)
+
     [<FunctionName("List")>]
     let list([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route="List/{type}")>] req: HttpRequest, ``type``:string, log: ILogger) =
       let t = ``type``
       log.LogInformation(sprintf "Listing '%s'" t)
-      JsonResult(store |> Seq.map (function KeyValue(_, row) -> row.value))
+      JsonResult(myStores req |> Seq.map (function KeyValue(_, row) -> row.value))
 
     [<FunctionName("Save")>]
     let save([<HttpTrigger(AuthorizationLevel.Anonymous, "post", Route="Save/{type}/{name}")>] req: HttpRequest, ``type``: string, name: string, log: ILogger) : ActionResult =
@@ -50,7 +69,7 @@ module API =
           upcast (ContentResult(Content="Must supply payload in request body", ContentType="text", StatusCode = Nullable 400))
         | Ok (Some v) ->
           let key = name
-          let v = ({ DynamicStorageRow.value = box v; key = key; owner = "Unknown"; ``type`` = t })
+          let v = ({ DynamicStorageRow.value = box v; key = key; owner = ident req; ``type`` = t })
           store <- (store |> Map.add key v)
           upcast JsonResult(v.value)
       with
@@ -62,7 +81,7 @@ module API =
     let load([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Load/{type}/{name}")>] req: HttpRequest, ``type``: string, name: string, log: ILogger) : ActionResult =
       let t = ``type``
       log.LogInformation(sprintf "Loading '%s' '%s'" t name)
-      match store.TryGetValue(name) with
+      match (myStores req).TryGetValue(name) with
       | true, v ->
         log.LogInformation(sprintf "Loaded '%s' '%s': '%A'" t name v)
         upcast JsonResult(v.value)
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 Generic key-value store built on top of Azure functions
 
-### Url: 
+### Usage examples: 
 
-(List)[http://wilsondata.azurewebsites.net/api/List]
+Authenticate via https://wilsondata.azurewebsites.net/.auth/login/Google (or Facebook or whatever) and then call one of the following:
+
+POST (Save)[https://wilsondata.azurewebsites.net/api/List/ssid/party1]
+GET (Load)[https://wilsondata.azurewebsites.net/api/Load/ssid/party1]
+GET (List)[https://wilsondata.azurewebsites.net/api/List/ssid]
+
+'ssid' represents a unique but arbitrary identifier chosen by the client app to identify a collection of data, 
+e.g. all data for the application "Shining Sword: the Impossible Dream" (ssid).
+
+If you're running in a browser (i.e. if UserAgent header is Mozilla/Chrome/etc.) authentication will be triggered automatically if you call an API endpoint 
+without already being authenticated. (You'll get a 302 to identity provider instead of a 401.)
 
 ### Run locally:
 ```
